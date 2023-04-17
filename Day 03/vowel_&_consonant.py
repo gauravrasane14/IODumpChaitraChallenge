6289d8c5fc71bb48998d1ac4626cb6bd71e27c35
Gaurav Rasane <124692215+gauravrasane14@users.noreply.github.com>
2023-04-17 22:52:53 +0530

    Create vowel_&_consonant.py

---
+++ Day 03/vowel_&_consonant.py
@@ -0,0 +1,30 @@
+#Question
+
+""" Program to check whether the given character is vowel consonant. """
+
+#Program
+
+ch = str(input("Enter the character to check: "))
+
+if (ch=="a" or ch=="e" or ch=="i" or ch=="o" or ch=="u" or ch=="A" or ch=="E" or ch=="I" or ch=="U"):
+
+    print("The character is Vowel.")
+
+else:
+
+    print("The character is Consonant.")
+
+#Output
+
+"""
+
+Enter the character to check: A
+
+The character is Vowel.
+
+Enter the character to check: h
+
+The character is Consonant.
+
+"""
+
