f369e242b00819302a2ecc11dd76d444dab1f2bf
Gaurav Rasane <124692215+gauravrasane14@users.noreply.github.com>
2023-04-20 11:57:16 +0530

    Create fahrenheit_to_celcius.py

---
+++ Day 06/fahrenheit_to_celcius.py
@@ -0,0 +1,17 @@
+# Question 
+
+"""
+Day 06 - Program to convert given temprature from Fahrenheit  to Celcius.
+""" 
+
+# Program
+
+tempF = int(input("Enter the temprature in Fahrenheit: "))
+tempC = (tempF - 32)*(5/9)
+print("Temperature in Celcius: ",tempC)
+# Output
+
+"""
+Enter the temprature in Celcius: 100
+Temperature in Celcius:  37.77777777777778
+"""
